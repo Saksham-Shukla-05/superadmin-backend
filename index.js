9322ee9d5d407e7339a28d85b88f5a6142bae0a4
Saksham-Shukla-05 <get467997@gmail.com>
2025-08-30 17:22:59 +0530

    feat : add swagger for api docs

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const auditLogRoutes = require("./routes/auditLogs");
 const analyticsRoutes = require("./routes/analytics");
 const cors = require("cors");
 const app = express();
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
 app.use(express.json());
 app.use(
   cors({
@@ -17,6 +20,27 @@ app.use(
   })
 );
 // Routes
+
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "SuperAdmin API",
+      version: "1.0.0",
+      description: "API documentation for SuperAdmin backend",
+    },
+    servers: [
+      {
+        url: "http://localhost:5000/api/v1/superadmin/",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/superadmin/users", userRoutes);
 app.use("/api/v1/superadmin/roles", roleRoutes);

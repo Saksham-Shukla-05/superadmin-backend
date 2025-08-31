08bf1b8492c6a49153c5ffec49c3dc2f6bf4025b
Saksham-Shukla-05 <get467997@gmail.com>
2025-08-31 22:04:20 +0530

    fix : refactor the index.js and some packages are added

---
+++ index.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+const express = require("express");
+const { requireSuperadmin } = require("./middlewares/auth");
+
+const userRoutes = require("./routes/users");
+const authRoutes = require("./routes/auth");
+const roleRoutes = require("./routes/roles");
+const auditLogRoutes = require("./routes/auditLogs");
+const analyticsRoutes = require("./routes/analytics");
+const cors = require("cors");
+const app = express();
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+// Routes
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/superadmin/users", userRoutes);
+app.use("/api/v1/superadmin/roles", roleRoutes);
+app.use("/api/v1/superadmin/audit-logs", auditLogRoutes);
+app.use("/api/v1/superadmin/analytics", analyticsRoutes);
+
+app.get("/", (req, res) => {
+  res.send("SuperAdmin API Running 🚀");
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
